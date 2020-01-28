5e1af36ea183ad3bfd3479a04207b8e622a30ddb
Jason Yue <yuehaiyin990427@gmail.com>
2020-01-28 23:25:45 +0000

    Use a nested class for access restriction

--- src/c4.5.cc
+++ src/c4.5.cc
@@ -1,42 +1,47 @@
 // For ortools-style logging and such
 #include "ortools/linear_solver/linear_solver.h"
 
-struct Node {
-  int atom; // 0 indicates a leaf node
-  union {
-    bool label;
-    Node *child[2];
-  } payload;
+class Tree {
+  struct Node {
+    int atom; // 0 indicates a leaf node
+    union {
+      bool label;
+      Node *child[2];
+    } payload;
 
-  // Constructs a leaf
-  Node(bool label): atom(0), payload{label} {}
+    // Constructs a leaf
+    Node(bool label): atom(0), payload{label} {}
 
-  // Constructs a non-leaf node
-  Node(int atom): atom(atom) {}
+    // Constructs a non-leaf node
+    Node(int atom): atom(atom) {}
 
-  ~Node() {
-    if (atom) {
-      delete payload.child[0];
-      delete payload.child[1];
+    ~Node() {
+      if (atom) {
+        delete payload.child[0];
+        delete payload.child[1];
+      }
     }
-  }
-};
-
-struct Tree {
+  };
   Node *root;
 
   // Note: only pass in dynamically allocated nodes
   Tree(Node *root): root(root) {}
 
+  public:
   ~Tree() {
     delete root;
+  }
+
+  static Tree test() {
+    Node* root = new Node(1);
+    root->payload.child[0] = new Node(true);
+    root->payload.child[1] = new Node(false);
+    Tree result(root);
+    return result;
   }
 };
 
 int main() {
-  Node* root = new Node(1);
-  root->payload.child[0] = new Node(true);
-  root->payload.child[1] = new Node(false);
-  Tree tree(root);
+  Tree tree = Tree::test();
   return EXIT_SUCCESS;
 }
