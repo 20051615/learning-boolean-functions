5824a2cccfcbb7e6d90dd19f738c5e416609040a
Jason Yue <yuehaiyin990427@gmail.com>
2020-05-21 01:59:13 +0800

    dimension:3, trunc:0.05, 59%

--- src/svm_kernel.cc
+++ src/svm_kernel.cc
@@ -4,7 +4,7 @@
 // TODO: i needs an integer type capable of storing 3^d
 
 const double EPSILON = 1.0e-6;
-const double W_TRUNC_FACTOR = 0.3;
+const double W_TRUNC_FACTOR = 0.05;
 
 #include "lpsvm.h"
 
